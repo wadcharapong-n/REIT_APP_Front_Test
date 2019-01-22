637ab1f968f97d0920dc1acd6b91e56799599ee2
nok <omerta@outlook.co.th>
2019-01-22 15:18:59 +0700

    format code

--- scrapy/reit_app_scrapy/reit_app_scrapy/pipelines.py
+++ scrapy/reit_app_scrapy/reit_app_scrapy/pipelines.py
@@ -10,6 +10,7 @@ from scrapy.conf import settings
 from scrapy.exceptions import DropItem
 from scrapy import log
 
+
 class ReitAppScrapyPipeline(object):
     def __init__(self):
         connection = pymongo.MongoClient(
@@ -19,7 +20,6 @@ class ReitAppScrapyPipeline(object):
         db = connection[settings['MONGODB_DB']]
         self.collection = db[settings['MONGODB_COLLECTION']]
 
-    
     def process_item(self, item, spider):
         valid = True
         for data in item:
@@ -27,41 +27,35 @@ class ReitAppScrapyPipeline(object):
                 valid = False
                 raise DropItem("Missing {0}!".format(data))
         if valid:
-            self.checkDuplicate(item, spider)       
+            self.checkDuplicate(item, spider)
         else:
             log.msg("REIT duplicate in MongoDB database!",
-                            level=log.DEBUG, spider=spider)
+                    level=log.DEBUG, spider=spider)
         return item
 
-    def checkDuplicate(self, item ,spider):
+    def checkDuplicate(self, item, spider):
 
         # print('Check Collecttion')
-        check_collection = self.collection.find({},{ "_id": 0}).count()
+        check_collection = self.collection.find({}, {"_id": 0}).count()
         if check_collection == 0:
             # print('Insert Collecttion')
             self.collection.insert(dict(item))
             log.msg("REIT added to MongoDB database!",
-                        level=log.DEBUG, spider=spider)
+                    level=log.DEBUG, spider=spider)
             return False
-
 
         # print('Check Contain')
         item_db = self.collection.find()
         for data in item_db:
             for key in data:
-                if key!='_id':
+                if key != '_id':
                     if data[key] != item[key]:
-                        myquery = { key: data[key] }
-                        newvalues = { "$set": { key: item[key] } }
+                        myquery = {key: data[key]}
+                        newvalues = {"$set": {key: item[key]}}
                         self.collection.update_one(myquery, newvalues)
                         # print('Update Collection')
-       
+
         log.msg("REIT updated to MongoDB database!",
-                        level=log.DEBUG, spider=spider)
+                level=log.DEBUG, spider=spider)
 
         return False
-        
-        
-
-        
-
