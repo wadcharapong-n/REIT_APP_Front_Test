0bf6649eb87366636478d7da44c045527e88b394
DESKTOP-VSIOK4A\Nok <omerta@outlook.co.th>
2019-01-22 14:26:49 +0700

    add get data from lhscreit

--- scrapy/reit_app_scrapy/reit_app_scrapy/items.py
+++ scrapy/reit_app_scrapy/reit_app_scrapy/items.py
@@ -4,7 +4,6 @@
 #
 # See documentation in:
 # https://doc.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 from scrapy.item import Item, Field
 
@@ -23,3 +22,18 @@ class ReitAppScrapyItem(scrapy.Item):
     address_trustee = Field()
     type_of_business = Field()
     auditor = Field()
+
+class ReitLhscAppScrapyItem(Item):
+    trust_name_th = Field()
+    establishment_date = Field()
+    registration_date = Field()
+    reit_manager = Field()
+    trust_tee = Field()
+    property_manager = Field()
+    investment_amount = Field()
+    investment_unit_amount = Field()
+    loan_amount = Field()
+    per_original_owner = Field()
+    per_general_public = Field()
+
+
--- scrapy/reit_app_scrapy/reit_app_scrapy/spiders/lhscreit.py
+++ scrapy/reit_app_scrapy/reit_app_scrapy/spiders/lhscreit.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import json
-# from reit_app_scrapy.items import ReitLhscAppScrapyItem
+from reit_app_scrapy.items import ReitLhscAppScrapyItem
 
 class QuotesSpider(scrapy.Spider):
     name = "lhscreit"
     start_urls = [
-        'lhscreit.com/porthilight',
+        'https://www.lhscreit.com/porthilight',
     ]
 
     def parse(self, response):
 
-        # td = response.css("td::text").extract()
+        td = response.css("td::text").extract()
         # trustName = td[0]
         # establishmentDate = td[1]
         # registrationDate = td[2]
@@ -23,22 +23,23 @@ class QuotesSpider(scrapy.Spider):
         # reitManager = td[8]
         # trusTee = td[9]
         # propertyManager = td[10]
-        # strong = response.css("strong::text").extract()
+        strong = response.css("strong::text").extract()
         # investmentAmount = strong[4]+strong[5]
         # investmentUnitAmount = strong[6]
         # loanAmount = strong[7]
         # perOriginalOwner = strong[10]
         # perGeneralPublic = strong[11]
 
-        # item = ReitLhscAppScrapyItem()
-        # item['trust_name_th'] = td[0]
-        # item['establishment_date'] = td[1]
-        # item['registration_date'] = td[2]
-        # item['reit_manager'] = td[8]
-        # item['trust_tee'] = td[9]
-        # item['property_manager'] = td[10]
-        # item['investment_amount'] = strong[4]+strong[5]
-        # item['investment_unit_amount'] = strong[6]
-        # item['loan_amount'] = strong[7]
-        # item['per_original_owner'] = strong[10]
-        # item['per_general_public'] = strong[11]
+        item = ReitLhscAppScrapyItem()
+        item['trust_name_th'] = td[0]
+        item['establishment_date'] = td[1]
+        item['registration_date'] = td[2]
+        item['reit_manager'] = td[8]
+        item['trust_tee'] = td[9]
+        item['property_manager'] = td[10]
+        item['investment_amount'] = strong[4]+strong[5]
+        item['investment_unit_amount'] = strong[6]
+        item['loan_amount'] = strong[7]
+        item['per_original_owner'] = strong[10]
+        item['per_general_public'] = strong[11]
+        yield item
