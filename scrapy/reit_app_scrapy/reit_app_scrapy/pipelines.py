7e305a9e66afcc45811dfafbd8127ad9ad0e7551
v_wadcha <v_wadcha@ctlsd.set>
2019-01-25 23:35:45 +0700

    edit check Duplicate

--- scrapy/reit_app_scrapy/reit_app_scrapy/pipelines.py
+++ scrapy/reit_app_scrapy/reit_app_scrapy/pipelines.py
@@ -30,36 +30,39 @@ class ReitAppScrapyPipeline(object):
                 valid = False
                 raise DropItem("Missing {0}!".format(data))
         if valid:
-            self.checkDuplicate(item, spider)
+            if self.checkDuplicate(item, spider):
+                # check update field
+                log.msg("REIT found data in MongoDB database!",
+                                level=log.DEBUG, spider=spider)
+                dataInMongo = self.findDataInMongo(item, spider)
+                for key in dataInMongo:
+                    if key != '_id':
+                        if dataInMongo[key] != item[key]:
+                            myquery = {key: dataInMongo[key]}
+                            newvalues = {"$set": {key: item[key]}}
+                            self.collection.update_one(myquery, newvalues)
+                            log.msg("REIT updated data in MongoDB database!",
+                                level=log.DEBUG, spider=spider)
+            else:
+                self.collection.insert(dict(item))
+                log.msg("REIT added data to MongoDB database!",
+                    level=log.DEBUG, spider=spider)
         else:
-            log.msg("REIT duplicate in MongoDB database!",
+            log.msg("REIT invalid data in MongoDB database!",
                     level=log.DEBUG, spider=spider)
         return item
 
     def checkDuplicate(self, item, spider):
-
-        # print('Check Collecttion')
-        check_collection = self.collection.find({}, {"_id": 0}).count()
-        if check_collection == 0:
-            # print('Insert Collecttion')
-            self.collection.insert(dict(item))
-            log.msg("REIT added to MongoDB database!",
-                    level=log.DEBUG, spider=spider)
-            return False
-
-        # print('Check Contain')
-        item_db = self.collection.find()
-        for data in item_db:
-            for key in data:
-                if key != '_id':
-                    if data[key] != item[key]:
-                        myquery = {key: data[key]}
-                        newvalues = {"$set": {key: item[key]}}
-                        self.collection.update_one(myquery, newvalues)
-                        # print('Update Collection')
-
-        log.msg("REIT updated to MongoDB database!",
-                level=log.DEBUG, spider=spider)
-
+        data = self.findDataInMongo(item, spider)
+        if data is not None:
+            return True
+        
         return False
 
+    def findDataInMongo(self, item, spider):
+        item_db = self.collection.find()
+        for data in item_db:
+            if data['trust_name_th'] == item['trust_name_th']:
+                return data
+
+        return None
